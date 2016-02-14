d891dac43d82f47b25ddb424dde392593c6e6410
berkedel <akhmad.hadi@gmail.com>
2016-02-14 13:22:52 +0900

    Add streaming function

--- plugins/monitor.py
+++ plugins/monitor.py
@@ -2,18 +2,20 @@ import os
 import time
 import requests
 import json
+from websocket import create_connection
 from neb.plugins import Plugin
 
 class MonitorPlugin(Plugin):
     """Monitors your smart home
     monitor snap : Get a snapshot from your smart home camera
-    monitor stream : Get a live stream from your smart home camera
+    monitor stream [start|stop]: Get a live stream from your smart home camera
     """
     name = "monitor"
 
     def cmd_snap(self, event, *args):
         # capture image from the camera module
-        rnd = "%s.jpg" % long(time.time())
+        t = long(time.time() * 1000)
+        rnd = "%s.jpg" % t
         fname = "/tmp/%s" % rnd
         cmd = "raspistill -o %s" % fname
         os.popen(cmd)
@@ -38,7 +40,20 @@ class MonitorPlugin(Plugin):
             'url': mxc['content_uri']
         }
         self.matrix.send_message_event(event['room_id'], event['type'], content)
-        return mxc['content_uri']
+        return "Snapshot at %s" % t
 
-    def cmd_stream(self, event, *args):
-        return "cmd_stream"
+    def cmd_stream(self, event, mode):
+        if mode == "start":
+            room = "r%s" % long(time.time() * 1000)
+            cmd = "curl -s 'http://127.0.0.1:8080/xmpp?server=meet.jit.si&port=5222&muc=conference.meet.jit.si&room=%s&room_password=&username=homebot&password=&bosh_enable=1&bosh_tls=1&bosh_server=meet.jit.si&bosh_port=443&bosh_hostname=meet.jit.si&reconnect=1&action=Start' > /dev/null" % room
+            os.popen(cmd)
+            return "Join room at http://meet.jit.si/%s" % room
+        elif mode == "stop":
+            cmd = "curl -s 'http://127.0.0.1:8080/xmpp?action=Stop' > /dev/null"
+            os.popen(cmd)
+            return "Streaming was stopped"
+        return "Mode is not recognized"
+
+    def on_event(self, event, event_type):
+        print(event_type)
+        return event_type
